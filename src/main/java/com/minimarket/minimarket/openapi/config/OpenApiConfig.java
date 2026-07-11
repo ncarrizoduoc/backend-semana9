3724674ea3d4de932eea5f4ec0229f9fb0379297
ncarrizoduoc <ni.carrizoc@duocuc.cl>
2026-07-10 22:38:52 -0400

    Se consolida configuracion de OpenAPI en una sola clase

--- src/main/java/com/minimarket/minimarket/openapi/config/OpenApiConfig.java
+++ src/main/java/com/minimarket/minimarket/openapi/config/OpenApiConfig.java
@@ -1,9 +1,14 @@
 package com.minimarket.minimarket.openapi.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+
+import org.springframework.beans.factory.annotation.Value;
+import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
 @SecurityScheme(
@@ -14,5 +19,15 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
     description = "Autenticacion y autorizacion por roles basada en JWT"
 )
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("0.0.1-SNAPSHOT") String appVersion){
+        return new OpenAPI()
+            .info(new Info()
+                .title("Minimarket")
+                .version(appVersion)
+                .description("Sistema de gestion de usuarios, productos y ventas de minimarket")
+            );
+    }
 
 }
