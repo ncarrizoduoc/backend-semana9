7473cb0315acda6dba6e86aa91c1214e2b8dab87
ncarrizoduoc <ni.carrizoc@duocuc.cl>
2026-07-14 17:34:30 -0400

    Se modifica logica de guardado de DetalleVenta

--- src/main/java/com/minimarket/minimarket/service/impl/DetalleVentaServiceImpl.java
+++ src/main/java/com/minimarket/minimarket/service/impl/DetalleVentaServiceImpl.java
@@ -1,8 +1,15 @@
 package com.minimarket.minimarket.service.impl;
 
 import com.minimarket.minimarket.entity.DetalleVenta;
+import com.minimarket.minimarket.entity.Producto;
+import com.minimarket.minimarket.exception.ResourceNotFoundException;
+import com.minimarket.minimarket.exception.StockInsuficienteException;
 import com.minimarket.minimarket.repository.DetalleVentaRepository;
+import com.minimarket.minimarket.repository.ProductoRepository;
 import com.minimarket.minimarket.service.DetalleVentaService;
+
+import jakarta.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +20,9 @@ public class DetalleVentaServiceImpl implements DetalleVentaService {
 
     @Autowired
     private DetalleVentaRepository detalleVentaRepository;
+
+    @Autowired
+    private ProductoRepository productoRepo;
 
     @Override
     public List<DetalleVenta> findAll() {
@@ -25,17 +35,38 @@ public class DetalleVentaServiceImpl implements DetalleVentaService {
     }
 
     @Override
+    @Transactional
     public DetalleVenta save(DetalleVenta detalleVenta) {
+        Producto producto = detalleVenta.getProducto();
+        producto.setStock(producto.getStock() - detalleVenta.getCantidad());
+        validarStock(producto);
+
         return detalleVentaRepository.save(detalleVenta);
     }
 
     @Override
+    @Transactional
     public void deleteById(Long id) {
+        DetalleVenta detalle = detalleVentaRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("No existe el detalle de venta con el ID ingresado"));
+
+        // Actualizar stock del producto
+        Producto producto = detalle.getProducto();
+        producto.setStock(producto.getStock() + detalle.getCantidad());
+        productoRepo.save(producto);
+        
+        // Eliminar detalle de venta
         detalleVentaRepository.deleteById(id);
     }
 
     @Override
     public List<DetalleVenta> findByVentaId(Long ventaId) {
         return detalleVentaRepository.findByVentaId(ventaId);
+    }
+
+    public void validarStock(Producto producto){
+        if (producto.getStock() < 0){
+            throw new StockInsuficienteException("No hay suficiente stock del producto: " + producto.getNombre());
+        }
     }
 }
