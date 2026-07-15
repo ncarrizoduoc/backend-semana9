873fe865ae1a5c27d97dfb3cd5f437f43c6f4f59
ncarrizoduoc <ni.carrizoc@duocuc.cl>
2026-07-15 01:54:21 -0400

    Se elimina dependencia no usada

--- src/main/java/com/minimarket/minimarket/config/DataInitializer.java
+++ src/main/java/com/minimarket/minimarket/config/DataInitializer.java
@@ -3,7 +3,6 @@ package com.minimarket.minimarket.config;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
